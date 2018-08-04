daf7dc151d89fde785f69e7f3b9a0c1b05988e7a
Takahito Yoneda <takahitoyoneda@kazuhiroyoneda-no-macbook-pro.local>
2018-08-04 15:04:20 +0900

    Initial Commit

---
+++ Podcast2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podcast2
+//
+//  Created by takahitoyoneda on 2018/08/04.
+//  Copyright © 2018 takahitoyoneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
